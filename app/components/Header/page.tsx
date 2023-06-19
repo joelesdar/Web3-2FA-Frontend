992904fefb7ed5d7c06256bdad777ee0a9ef4c85
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-06-18 22:29:53 -0500

    Header terminado y responsive para todos los tamaños, primera sección de la landing hecha para tamaño movil

--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -1,14 +1,57 @@
 'use client'
 import Link from "next/link"
 import { StyledHeader } from "./styles";
-import { use } from "react";
+import Image from "next/image";
+import { useState } from "react";
+import menuIcon from "../..//../public/Menu icon.svg";
+import closeIcon from "../..//../public/Close icon.svg";
 
 const Header = () => {
+
+  const [showButton, setShowButton] = useState(true);
+
+  const toggleButton = () => {
+    setShowButton(!showButton);
+    console.log(showButton)
+  }
+
   return (
     <StyledHeader>
-      <Link href="./../../Demo">Demo</Link>
+      <Link href="./../../Demo">Web3-2FA</Link>
+      <div className="menu-icon">
+        {showButton && 
+        <a onClick={toggleButton}>
+          <Image
+            priority
+            src={menuIcon}
+            alt="Menu"
+            id="menu"
+          />
+        </a>}
+        {!showButton && 
+        <a onClick={toggleButton}>
+          <Image
+            priority
+            src={closeIcon}
+            alt="Menu"
+            id="close"
+          />
+        </a>}
+      </div>
+      {!showButton &&<div className="menu">
+         <ul>
+          <li><Link href="https://github.com/joelesdar/Web3-2FA-Backend" target="_blank">Repository</Link></li>
+          <li><Link href="https://github.com/joelesdar/Web3-2FA-Backend" target="_blank">NPM Package</Link></li>
+          <li><Link href="./../../Demo">Demo</Link></li>
+        </ul>
+      </div>}
+      <div className="navbar">
+        	<Link href="https://github.com/joelesdar/Web3-2FA-Backend" target="_blank">Repository</Link>
+          <Link href="https://github.com/joelesdar/Web3-2FA-Backend" target="_blank">NPM Package</Link>
+          <Link href="./../../Demo">Demo</Link>
+      </div>
     </StyledHeader>
-   );
+  );
 }
  
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from 'next/image'
+'use client'
 import Header from './components/Header/page'
+import Landing from './Landing/page'
 import Link from 'next/link'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className='Header'>
       <Header />
+      <Landing />
     </div>
   )
 }
