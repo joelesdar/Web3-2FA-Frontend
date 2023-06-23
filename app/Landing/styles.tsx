e978c6a41186b40e2626d0e7e94a0c6bf61b71d1
Joel Esteban Diaz Arévalo <joel-e11@hotmail.com>
2023-06-22 19:13:50 -0500

    Landing page en formato movil lista

--- app/Landing/styles.tsx
+++ app/Landing/styles.tsx
@@ -8,6 +8,9 @@ export const StyledLanding = styled.div`
   justify-content: center;
   text-align: center;
   padding-top: 56px;
+  flex-direction: column;
+  padding-left: 16px;
+  padding-right: 16px;
 
   img {
     /* width: 60%; */
@@ -19,11 +22,39 @@ export const StyledLanding = styled.div`
     font-size: 56px;
     font-weight: 700;
     margin-top: 32px;
+    color: #CBE4DE;
+    text-shadow: 0 0 10px #CBE4DE;
   }
 
   p {
     font-weight: 400;
     margin: 0 16px;
+    line-height: 36px;
+  }
+
+  span {
+    color: #0E8388;
+  }
+
+  iframe {
+    width: 100%;
+    height: 360px;
+    background: #2C3333;
+    border-radius: 16px;
+  }
+  
+  .section {
+    padding-top: 64px;
+    ol {
+      text-align: start;
+    }
+  }
+
+  .section-title {
+    color: #CBE4DE;
+    font-size: 24px;
+    font-weight: 700;
+    padding-bottom: 32px;
   }
   
   .subtitle {
@@ -34,6 +65,13 @@ export const StyledLanding = styled.div`
   .description {
     font-size: 18px;
     margin-top: 80px;
+  }
+
+  .docs {
+    font-weight: 200;
+    font-size: 16px;
+    margin-top: 40px;
+    padding-bottom: 80px;
   }
 
   button {
